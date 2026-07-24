f806ff7f32fce298602f71aa4ed82f4697fa56dd
Phathutshedzotshivhenga <25088116@mvula.univen.ac.za>
2026-07-24 15:45:09 +0200

    Update Dev.java

--- src/main/java/com/mycompany/dev/Dev.java
+++ src/main/java/com/mycompany/dev/Dev.java
@@ -8,7 +8,7 @@ import javax.swing.JOptionPane;
 public class Dev {
 
     public static void main(String[] args) {
-     J0ptionpane.ShowInputDialog(null,"Enter any number from 1-10");
+     J0ptionpane.ShowMessageDialog(null,"Enter any number from 1-10");
      JOptionpane.ShowMessageDialog(null,"The number is"+(1+int)(math.random(*10);
      
     }
