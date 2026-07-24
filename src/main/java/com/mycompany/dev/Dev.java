31b94e508fe0686438625e34d074daee0a258a39
Phathutshedzo Tshivhenga <25088116@mvula.univen.ac.za>
2026-07-24 15:43:01 +0200

    First commit

---
+++ src/main/java/com/mycompany/dev/Dev.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.dev;
+
+import javax.swing.JOptionPane;
+public class Dev {
+
+    public static void main(String[] args) {
+     J0ptionpane.ShowInputDialog(null,"Enter any number from 1-10");
+     JOptionpane.ShowMessageDialog(null,"The number is"+(1+int)(math.random(*10);
+     
+    }
+}
---
+++ src/main/java/com/mycompany/dev/J0ptionpane.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.dev;
+
+/**
+ *
+ * @author 25088116
+ */
+class J0ptionpane {
+
+    static void ShowInputDialog(Object object, String enter_any_number_from_110) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
